dea36f8704769476bda59b7d4184c144932696cc
jiyoungkim <kjy09221@naver.com>
2022-05-11 11:35:16 +0900

    new file

--- bk-airline/src/main/java/com/ezen/bk/HomeController.java
+++ bk-airline/src/main/java/com/ezen/bk/HomeController.java
@@ -1,6 +1,7 @@
 package com.ezen.bk;
 
 import java.text.DateFormat;
+
 import java.util.Date;
 import java.util.Locale;
 
@@ -10,30 +11,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
+
+	@RequestMapping(value="/")
+	public String maina() {
+		return "main";
+	}
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
+	@RequestMapping(value="/index")
+	public String mainb() {
+		return "main";
+	}
+	/*
+	 * 이용약관, 개인정보처리방침
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	@RequestMapping("/tou")
+	public String tou() {
+		return "tou";
+	}
+	@RequestMapping("/private")
+	public String private1() {
+		return "private";
 	}
 	
 }
